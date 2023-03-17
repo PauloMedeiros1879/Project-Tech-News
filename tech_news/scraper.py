537540ab66c6deb06e8e36e66b8cbebb194dc6f0
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-03-17 12:07:58 -0400

    feat(scraper.py) Implementa na função scrape_next_page_link um scrape para obter a url da proxima página e caso não encontre, retorna None REQ03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,6 +28,12 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+
+    next_page = selector.css(".next.page-numbers::attr(href)").get()
+    if next_page:
+        return next_page
+    return None
 
 
 # Requisito 4
